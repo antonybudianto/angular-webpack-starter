22e658b35959dde0dc9d8e06186c64d12efe81d2
Antony Budianto <antonybudianto@gmail.com>
2016-09-25 18:38:06 +0700

    remove unused typing declaration

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,8 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-declare var process: any;
-
 if (process.env.APP_ENV === 'production') {
     enableProdMode();
 }
