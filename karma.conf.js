bb9e421517c916c4142b31de12fcac0cc62e37f5
Antony <antony.budianto@gdplabs.id>
2016-10-19 17:45:37 +0700

    remove unused package

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
   var _config = {
     basePath: '',
 
-    frameworks: ['jasmine', 'source-map-support'],
+    frameworks: ['jasmine'],
 
     files: [
       {
