09349494ee6676af59ffeea9a55f81de6df724ed
Antony Budianto <antonybudianto@gmail.com>
2016-11-03 22:02:30 +0700

    move global js lib to vendor

--- src/vendor-style.ts
+++ src/vendor-style.ts
@@ -1,4 +1,2 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap';
--- src/vendor.ts
+++ src/vendor.ts
@@ -9,6 +9,10 @@ import '@angular/router';
 // Uncomment below if you use RxJS in many non-preload lazy-loaded modules
 // import 'rxjs';
 
+// Global packages
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+
 // Development only packages, for faster build with DLL
 if (process.env.APP_ENV === 'development') {
     require('@angular/platform-browser-dynamic');
