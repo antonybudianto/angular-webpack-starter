80fa0b41111f89086cacc5955d532a7fcf872376
Antony Budianto <antonybudianto@gmail.com>
2016-11-10 06:39:49 +0700

    include all ts source for full coverage

--- config/test/karma-test-shim.js
+++ config/test/karma-test-shim.js
@@ -11,7 +11,7 @@ require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 
-var appContext = require.context('../../src', true, /\.spec\.ts/);
+var appContext = require.context('../../src/app', true, /\.ts/);
 
 appContext.keys().forEach(appContext);
 
